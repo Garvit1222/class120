ea384d9e79cb6f83ef58519675c3870c3cbdb837
Garvit1222 <121927843+Garvit1222@users.noreply.github.com>
2023-04-28 19:56:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier('mobilenet' , modelloaded);
+}
+
+function modelloaded()
+{
+   console.log('modelloaded');
+}
+
+function draw()
+{
+  image(video, 0,0,300,300);
+  classifier.classify(video,gotresult);
+ 
+}
+
+prv_result = '' ;
+
+function gotresult(error,results)
+{
+  if (error)
+  {
+    console.error(error);
+  }
+  
+  else
+  {
+    if((results[0].confidence >= 0.5)&&(prv_results != results[0].label))
+    {
+    console.log(results);
+
+    prv_result = result[0].label;
+    var Synth = window.speechSynthesis;
+    speak_data = 'object detected is' + results[0].label;
+    Utterthis = new SpeechSynthesisUtterance(speak_data);
+    Synth.speak(Utterthis);
+
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(2);
+    }
+  }
+}
+
